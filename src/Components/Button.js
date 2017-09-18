7979ef48e1429fe780be251828c1c2a48eba5ff3
Kyle Holmberg <kylemh.email@gmail.com>
2017-09-17 23:29:06 -0700

    Add button component

---
+++ src/Components/Button.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Button(props) {
+  return (
+    <button
+      onClick={props.clickAction}
+      disabled={props.isDisabled}
+      aria-label={`${props.text} button`}
+    >
+      {props.text}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  text: PropTypes.string.isRequired,
+  clickAction: PropTypes.func.isRequired,
+  isDisabled: PropTypes.bool,
+};
+
+Button.defaultTypes = {
+  isDisabled: false,
+}
+
+export default Button;
