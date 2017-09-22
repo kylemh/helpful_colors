a8d811bee0e6f597220f15a1beebe9963843e42a
Kyle Holmberg <kylemh.email@gmail.com>
2017-09-22 00:57:34 -0700

    Customize 404, BEMify Header.styl, Add global styles

--- src/Components/NotFound.js
+++ src/Components/NotFound.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import SadPanda from '../Images/sad_panda.png';
 
 export default class NotFound extends React.Component {
   render () {
     return (
-      <div>
-        <h1>404 </h1>
-        <h2>Page Not Found</h2>
+      <div className="router-view__contents not-found-view">
+        <header className="not-found-view__header">
+          <h1>404 Error</h1>
+          <h2>Page Not Found</h2>
+        </header>
+        <br />
+        <img src={SadPanda} alt="A sad panda" />
+        <br />
         <p>Sorry, but the page you are looking for does not exist.</p>
       </div>
     )
